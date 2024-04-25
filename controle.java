7cf8d5e701aff916800f7cfcb7f9474d0a6863bc
João Victor <154941418+ThisisJovi@users.noreply.github.com>
2024-04-24 21:14:44 -0300

    Create controle.java

---
+++ controle.java
@@ -0,0 +1,25 @@
+public class controle {
+    public static void main (String[] args) {
+        controle controle = new controle ();
+
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+        controle.setCanal (499);
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+        controle.addCanal();
+    }
+
+    private void setCanal(int i) {
+    }
+
+    private void addCanal() {
+    }
+    
+}
